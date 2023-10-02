1410f1d393e8302e9e3b88b6e0f37b8752aadecb
Menzy <Joshuamensah46@gmail.com>
2023-10-02 23:35:10 +0000

    Updated portfolio website

--- personal-portfolio-website/myWebsiteIndex.js
+++ personal-portfolio-website/myWebsiteIndex.js
@@ -1,28 +1,46 @@
-const modeToggle = document.getElementById("toggle-mode");
-const body = document.body;
+// const modeToggle = document.getElementById("toggle-mode");
+// const body = document.body;
 
-// Check the initial state of the dark mode
-if (localStorage.getItem("dark-mode") === "enabled") {
-	enableDarkMode();
-} else {
-	disableDarkMode();
-}
+// // Check the initial state of the dark mode
+// if (localStorage.getItem("dark-mode") === "enabled") {
+// 	enableDarkMode();
+// } else {
+// 	disableDarkMode();
+// }
 
-// Toggle dark mode on click
-modeToggle.addEventListener("click", () => {
-	if (body.classList.contains("dark-mode")) {
-		disableDarkMode();
-	} else {
-		enableDarkMode();
-	}
+// // Toggle dark mode on click
+// modeToggle.addEventListener("click", () => {
+// 	if (body.classList.contains("dark-mode")) {
+// 		return disableDarkMode();
+// 	}
+
+// 	enableDarkMode();
+// });
+
+// function enableDarkMode() {
+// 	body.classList.add("dark-mode");
+// 	localStorage.setItem("dark-mode", "enabled");
+// }
+
+// function disableDarkMode() {
+// 	body.classList.remove("dark-mode");
+// 	localStorage.setItem("dark-mode", "disabled");
+// }
+
+const lightModeButton = document.querySelector("#light-mode-button");
+const darkModeButton = document.querySelector("#dark-mode-button");
+const html = document.querySelector("html");
+
+lightModeButton.addEventListener("click", () => {
+	// Toggle to dark theme
+	html.setAttribute("data-theme", "dark");
+	lightModeButton.style.display = "none";
+	darkModeButton.style.display = "block";
 });
 
-function enableDarkMode() {
-	body.classList.add("dark-mode");
-	localStorage.setItem("dark-mode", "enabled");
-}
-
-function disableDarkMode() {
-	body.classList.remove("dark-mode");
-	localStorage.setItem("dark-mode", "disabled");
-}
+darkModeButton.addEventListener("click", () => {
+	// Toggle to light theme
+	html.setAttribute("data-theme", "light");
+	darkModeButton.style.display = "none";
+	lightModeButton.style.display = "block";
+});
